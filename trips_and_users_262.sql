88649da882dd36c6384f75e4fe310915a755aac6
zsun <sunzhongyuan888@aliyun.com>
2017-02-25 11:41:48 +0800

    improve sql

--- trips_and_users_262.sql
+++ trips_and_users_262.sql
@@ -1,10 +1,12 @@
-select
-t.request_at as "Day",
-round(sum(if(t.Status in ("cancelled_by_client", "cancelled_by_driver"), 1, 0))/count(1), 2) as "Cancellation Rate"
+# Write your MySQL query statement below
+select t.Request_at as "Day", 
+-- round(sum(case when t.Status = "cancelled_by_client" or t.Status = "cancelled_by_driver" then 1 else 0 end)/count(1), 2) as "Cancellation Rate"
+round(sum(if (t.Status in ("cancelled_by_client", "cancelled_by_driver"), 1, 0))/count(1), 2) as "Cancellation Rate"
+# truncate 不行，没有四舍五入了
+# format 不行, 返回的是字符串而不是数字了
 
 from Trips t
-join Users u on t.client_id = u.users_id
-where u.banned = "No" and u.Role = "client"
-and t.request_at >= "2013-10-01" and t.request_at <= "2013-10-03"
-group by t.request_at
-order by t.request_at
+join Users u on t.Client_Id = u.Users_Id and u.Role = "client" and u.Banned = "No"
+where t.Request_at between "2013-10-01" and "2013-10-03"
+group by t.Request_at
+order by t.Request_at
