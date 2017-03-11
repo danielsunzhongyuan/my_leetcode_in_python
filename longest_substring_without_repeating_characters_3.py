4109f88f388fd3e2109bac68790172ca975003e1
zsun <sunzhongyuan888@aliyun.com>
2017-03-11 20:46:17 +0800

    another solution for #3

--- longest_substring_without_repeating_characters_3.py
+++ longest_substring_without_repeating_characters_3.py
@@ -7,11 +7,28 @@ class Solution(object):
         length = len(s)
         if length <= 1:
             return length
+        # Solution 1
+        # res = 1
+        # slow_index = 0
+        # for i in range(1, length):
+        #     if s[i] in s[slow_index:i]:
+        #         slow_index += s[slow_index:i+1].index(s[i]) + 1
+        #     if res < i - slow_index + 1:
+        #         res = i - slow_index + 1
+        # return res
+        
+        # Solution 2
         res = 1
+        charactor_index_mapping = {}
         slow_index = 0
+        charactor_index_mapping[s[0]] = 0
         for i in range(1, length):
-            if s[i] in s[slow_index:i]:
-                slow_index += s[slow_index:i+1].index(s[i]) + 1
-            if res < i - slow_index + 1:
-                res = i - slow_index + 1
+            if charactor_index_mapping.get(s[i], -1) > -1 and charactor_index_mapping.get(s[i]) >= slow_index:
+                if res < i - slow_index:
+                    res = i - slow_index
+                slow_index = charactor_index_mapping.get(s[i]) + 1
+            else:
+                if res < i - slow_index + 1:
+                    res = i - slow_index + 1
+            charactor_index_mapping[s[i]] = i
         return res
