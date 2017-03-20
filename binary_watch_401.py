e8de2315735e0850d05b584676efd94a9f838aba
zsun <sunzhongyuan888@aliyun.com>
2017-03-20 16:20:39 +0800

    improve

---
+++ binary_watch_401.py
@@ -0,0 +1,37 @@
+# @question: 401. Binary Watch
+# @author: Zhongyuan Sun
+# Solution One: split num into num_of_hour and num_of_minute like (0, num), (1, num-1), (2, num-2) ...
+#               then generate all the possibilities of hour and minute
+#               then return the combination of hour and minute
+# Solution Two: put hour digit and minute digit together, generate all the permutations
+#               then read the number as hour:minute
+# Solution Three: based on solution two, instead of using itertools.product(("0","1")...) to generate the permutations,
+#               use numbers [0, 2**10) to represent the permutation
+
+class Solution(object):
+    def readBinaryWatch(self, num):
+        """
+        :type num: int
+        :rtype: List[str]
+        """
+        # Solution One
+        # res = []
+        # for i in range(min(4,num+1)):
+        #     res += [":".join(x) for x in itertools.product(list(map(lambda x:str(x),filter(lambda x3: x3<12, map(lambda x1:int("".join(x1), 2), filter(lambda x2:x2.count("1") == i,itertools.product(("0","1"),("0","1"),("0","1"),("0","1"))))))),list(map(lambda x:"%02d"%x,filter(lambda x3: x3<60, map(lambda x1:int("".join(x1), 2), filter(lambda x2:x2.count("1") == num-i,itertools.product(("0","1"),("0","1"),("0","1"),("0","1"),("0","1"),("0","1"))))))))]
+        # return res
+        
+        # Solution One: one line version
+        # return list(y for x in [[":".join(x) for x in itertools.product(list(map(lambda x:str(x),filter(lambda x3: x3<12, map(lambda x1:int("".join(x1), 2), filter(lambda x2:x2.count("1") == i,itertools.product(("0","1"),("0","1"),("0","1"),("0","1"))))))),list(map(lambda x:"%02d"%x,filter(lambda x3: x3<60, map(lambda x1:int("".join(x1), 2), filter(lambda x2:x2.count("1") == num-i,itertools.product(("0","1"),("0","1"),("0","1"),("0","1"),("0","1"),("0","1"))))))))] for i in range(min(4,num+1))] for y in x)
+        
+        # Solution One: one line version, improved
+        return list(y for x in [[":".join(x) for x in itertools.product(list(map(lambda x:str(x),filter(lambda x3: x3<12, map(lambda x1:int("".join(x1), 2), filter(lambda x2:x2.count("1") == i,itertools.product(*[('0','1')]*4)))))),list(map(lambda x:"%02d"%x,filter(lambda x3: x3<60, map(lambda x1:int("".join(x1), 2), filter(lambda x2:x2.count("1") == num-i,itertools.product(*[('0','1')]*6)))))))] for i in range(min(4,num+1))] for y in x)
+        
+        # Solution Two: one line version
+        # return ["%d:%02d"%(x[0],x[1]) for x in [(int("".join(x[:4]), 2), int("".join(x[4:]), 2)) for x in filter(lambda x:x.count("1")==num, itertools.product(("0","1"),("0","1"),("0","1"),("0","1"),("0","1"),("0","1"),("0","1"),("0","1"),("0","1"),("0","1")))] if x[0]<12 and x[1]<60]
+
+        # Solution Two: one line version, improved
+        # return ["%d:%02d"%(x[0],x[1]) for x in [(int("".join(x[:4]), 2), int("".join(x[4:]), 2)) for x in filter(lambda x:x.count("1")==num, itertools.product(*[('0','1')]*10))] if x[0]<12 and x[1]<60]
+        
+        # Solution Three
+        # return ["%d:%02d"%(x[0],x[1]) for x in [(int("".join(x[:4]), 2), int("".join(x[4:]), 2)) for x in filter(lambda x:x.count("1")==num, ["%010d"%int(bin(x)[2:],10) for x in range(2**10)])] if x[0]<12 and x[1]<60]
+
---
+++ lowest_common_ancestor_of_a_binary_tree_236.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        # Solution One: 99ms
+        # stack = [root]
+        # parent = {root: None}
+        # while p not in parent or q not in parent:
+        #     node = stack.pop()
+        #     if node.left:
+        #         parent[node.left] = node
+        #         stack.append(node.left)
+        #     if node.right:
+        #         parent[node.right] = node
+        #         stack.append(node.right)
+        # ancestor_of_p = []
+        # while p:
+        #     ancestor_of_p.append(p)
+        #     p = parent[p]
+        # while q not in ancestor_of_p:
+        #     q = parent[q]
+        # return q
+
+        # Solution Two:
+        if root in (None, p, q):
+            return root
+        left, right = (self.lowestCommonAncestor(kid, p, q) for kid in (root.left, root.right))
+        return root if left and right else left or right
