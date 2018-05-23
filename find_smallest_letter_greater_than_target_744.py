41be2663ed28514e2a5c268d695a7da7f0f2d415
sunzhongyuan <sunzhongyuan@lvwan.com>
2018-05-23 15:13:55 +0800

    improve

--- find_smallest_letter_greater_than_target_744.py
+++ find_smallest_letter_greater_than_target_744.py
@@ -5,18 +5,22 @@ class Solution(object):
         :type target: str
         :rtype: str
         """
-        length = len(letters)
-        left, right = 0, length - 1
-        while left < right:
-            mid = (left+right) / 2
-            print left, mid, right
-            if letters[mid] > target:
-                right = mid - 1
-            else:
-                left = mid + 1
-        if letters[left] <= target:
-            return letters[(left+1)%length]
-        return letters[left]
+        # length = len(letters)
+        # left, right = 0, length - 1
+        # while left < right:
+        #     mid = (left+right) / 2
+        #     print left, mid, right
+        #     if letters[mid] > target:
+        #         right = mid - 1
+        #     else:
+        #         left = mid + 1
+        # if letters[left] <= target:
+        #     return letters[(left+1)%length]
+        # return letters[left]
+
+        import bisect
+        idx = bisect.bisect_right(letters, target)
+        return letters[idx % len(letters)]
 
 def main():
 	s = Solution()
