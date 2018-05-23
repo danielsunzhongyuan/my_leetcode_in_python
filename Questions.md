5f099b6a590c78ff93eff3cbc2b564b43e3ece77
sunzhongyuan <sunzhongyuan@lvwan.com>
2018-05-23 15:42:22 +0800

    :sparkles: 最长升序串的长度

--- Questions.md
+++ Questions.md
@@ -832,6 +832,21 @@ For example, if there are 4 stones in the heap, then you will never win the game
 """
 
 
+300. longest-increasing-subsequence
+Given an unsorted array of integers, find the length of longest increasing subsequence.
+
+Example:
+
+Input: [10,9,2,5,3,7,101,18]
+Output: 4 
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4. 
+Note:
+
+There may be more than one LIS combination, it is only necessary for you to return the length.
+Your algorithm should run in O(n2) complexity.
+Follow up: Could you improve it to O(n log n) time complexity?
+
+
 313. super-ugly-number
 Write a program to find the nth super ugly number.
 
---
+++ longest_increasing_subsequence_300.py
@@ -0,0 +1,54 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # length = len(nums)
+        # if length == 0:
+        #     return 0
+        # longest_increasing_number = [0]*length
+        # longest_increasing_number[0] = 1
+        # for i in range(1, length):
+        #     before_max = 0
+        #     for j in range(i):
+        #         if nums[j] < nums[i] and longest_increasing_number[j] > before_max:
+        #             before_max = longest_increasing_number[j]
+        #     longest_increasing_number[i] = 1 + before_max
+        # return max(longest_increasing_number)
+
+        # 这个算法的精髓在于：
+        # res 数组并不是真正的保存结果，即最长升序串，而是让最长升序串把这个数组撑起来
+        # 最终从res的长度得知，最长升序串的长度
+
+        # 如果后面的数，比res最后的数大，就填进去，
+        # 如果后面的数，小于等于res第一个数，就换一下
+        # 否则，就在 res 里找到响应的位置，替换进去。
+        # 假如nums数组是 [10，100，101，102，11，12，13，14，2, 2，1]，那么res的变化是
+        # [10]
+        # [10, 100]
+        # [10, 100, 101]
+        # [10, 100, 101, 102]
+        # [10, 11, 101, 102]
+        # [10, 11, 12, 102]
+        # [10, 11, 12, 13]
+        # [10, 11, 12, 13, 14]
+        # [2, 11, 12, 13, 14]
+        # [2, 11, 12, 13, 14]
+        # [1, 11, 12, 13, 14]
+        # 最长升序串是 10，11，12，13，14，长度为
+        import bisect
+        if len(nums) < 2:
+            return len(nums)
+        res = [nums[0]]
+        for num in nums[1:]:
+            if(num == res[-1]):
+                continue
+            if(num > res[-1]):
+                res.append(num)
+            elif(num <= res[0]):
+                res[0] = num
+            else:
+                loc = bisect.bisect_left(res, num)
+                res[loc] = num
+        return len(res)
