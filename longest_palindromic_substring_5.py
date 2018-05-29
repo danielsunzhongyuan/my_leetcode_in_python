74712a1b821e545ddd25aa3258f4f8123266129d
sunzhongyuan <sunzhongyuan@lvwan.com>
2018-05-29 14:04:28 +0800

    最长的回文子串

--- longest_palindromic_substring_5.py
+++ longest_palindromic_substring_5.py
@@ -5,18 +5,30 @@ class Solution(object):
         :rtype: str
         """
         length = len(s)
-        if s == s[::-1]:
+        if length < 2 or s == s[::-1]:
             return s
         ret = ""
-        i = 0
-        for j in range(i + len(ret) + 1, length + 1):
-            if s[:j] == s[j - 1::-1]:
-                ret = s[:j]
-        for i in range(1, length):
+        for i in range(length):
             for j in range(i + len(ret) + 1, length + 1):
-                if s[i:j] == s[j - 1:i - 1:-1] and len(ret) < (j - i):
+                if s[i:j] == s[j - length - 1:i - length - 1:-1]:
                     ret = s[i:j]
         return ret
+
+        # if len(s) < 2 or s == s[::-1]:
+        #     return s
+        # n = len(s)
+        # start, maxlen = 0, 1
+        # for i in range(n):
+        #     odd = s[i - maxlen - 1:i + 1]  # len(odd)=maxlen+2
+        #     even = s[i - maxlen:i + 1]  # len(even)=maxlen+1
+        #     if i - maxlen - 1 >= 0 and odd == odd[::-1]:
+        #         start = i - maxlen - 1
+        #         maxlen += 2
+        #         continue
+        #     if i - maxlen >= 0 and even == even[::-1]:
+        #         start = i - maxlen
+        #         maxlen += 1
+        # return s[start:start + maxlen]
 
 
 def main():
