f788fea7c0872f133b8cbe80d7cbc8944b2246ff
sunzhongyuan <sunzhongyuan@lvwan.com>
2018-10-24 10:58:43 +0800

    improve

--- powx_n_50.py
+++ powx_n_50.py
@@ -5,12 +5,26 @@ class Solution(object):
         :type n: int
         :rtype: float
         """
-        result = x
+        # Solution One: 28ms
+        # result = x
+        # if n == 0:
+        #     return 1
+        # elif n < 0:
+        #     return 1 / self.myPow(x, -n)
+        # elif n % 2 == 1:
+        #     return x * self.myPow(x * x, n / 2)
+        # else:
+        #     return self.myPow(x * x, n / 2)
+        
+        # Solution Two: 20ms
         if n == 0:
-            return 1
-        elif n < 0:
-            return 1 / self.myPow(x, -n)
-        elif n % 2 == 1:
-            return x * self.myPow(x * x, n / 2)
+            return 1.0
+        if n < 0:
+            return 1.0/self.myPow(x, -n)
+        if n % 2 == 0:
+            result = self.myPow(x, n/2)
+            return result*result
         else:
-            return self.myPow(x * x, n / 2)
+            result = self.myPow(x, n/2)
+            return x*result*result
+
